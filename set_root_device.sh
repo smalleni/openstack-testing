ffad100dcbe79160f1a915e2db3da863ff3efc5b
Sai Sindhur Malleni <smalleni@redhat.com>
2017-02-24 20:42:08 -0500

    Set root device based on node type

--- set_root_device.sh
+++ set_root_device.sh
@@ -1,16 +1,19 @@
 export PASSWD=$(sudo crudini --get /etc/ironic-inspector/inspector.conf swift password)
 uuids=$(openstack baremetal node list --format value -c UUID)
-mkdir hardware
-pushd hardware > /dev/null
+node_type=pcloud
+mkdir $node_type
+pushd $node_type > /dev/null
 for uuid in $uuids; do
-    swift -q -U service:ironic -K $PASSWD download ironic-inspector inspector_data-$uuid 2> /dev/null
+    node=$(ironic node-show $uuid | grep $node_type)
+    if [ "$node" != 0 ]
+        then
+            swift -q -U service:ironic -K $PASSWD download ironic-inspector inspector_data-$uuid 2> /dev/null
+    fi
 done
 popd > /dev/null
-
-for f in $(ls ~/hardware/); do echo -n $f | sed s/inspector_data-//g; echo -n " "; cat ~/hardware/$f | jq '.inventory.disks' | grep -A 5 sda | grep wwn\" | tail -1 | awk {'print $2'} | sed -e s/,//g -e s/\"//g;  done > ~/uuid_to_wwn
-
+for f in $(ls ~/$node_type/); do echo -n $f | sed s/inspector_data-//g; echo -n " "; cat ~/$node_type/$f | jq '.inventory.disks' | grep -A 5 sda | grep wwn\" | tail -1 | awk {'print $2'} | sed -e s/,//g -e s/\"//g;  done > ~/uuid_to_wwn
 while read UUID WWN ; do
-      echo $UUID $WWN;
+      echo "Setting root disk on node $UUID with wwn $WWN"
       ironic node-update $UUID remove properties/root_device
       ironic node-update $UUID add properties/root_device="{\"wwn\": \"$WWN\"}"
 done < <(cat ~/uuid_to_wwn)
