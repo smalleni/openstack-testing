73a7737d8dd4c891772755fe2745691d22b93e11
Sai Sindhur Malleni <smalleni@redhat.com>
2017-02-27 22:02:09 -0500

    Write node details to file

--- ping_nodes.sh
+++ ping_nodes.sh
@@ -1,4 +1,5 @@
 source ~/stackrc
+rm -f reachable.txt intermittent.txt unreachable.txt > /dev/null 2>&1
 for ip in `openstack server list -f value -c Networks | sed s/ctlplane=//`
     do
        server=$(openstack server list | grep $ip | awk {'print$2'})
@@ -9,16 +10,16 @@ for ip in `openstack server list -f value -c Networks | sed s/ctlplane=//`
            count=$(ping -c 5 $ip | grep received | awk {'print $4'})
            if [ "$count" -eq 5 ]
                then
-                   echo Ironic node $node is pingable always
+                   echo Ironic node $node is pingable always >> reachable.txt
                    break
            elif [ "$count" -lt 5 && "$count" -gt 0]
                then
-                   echo Ironic node $node is pingable intermittantly
+                   echo Ironic node $node is pingable intermittently >> intermittent.txt
                    break
            else
                if [ "$TRY" -eq 1]
                    then
-                       echo Ironic node $node is not pingable
+                       echo Ironic node $node is not pingable >> unreachable.txt
                fi
            fi
            TRY=$(TRY+1)
