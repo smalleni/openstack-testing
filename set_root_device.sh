fef472415769bff3576ead5f25644172ff082126
Sai Sindhur Malleni <smalleni@redhat.com>
2018-05-17 13:58:39 -0600

    Use root_disk param from introspection data

--- set_root_device.sh
+++ set_root_device.sh
@@ -1,7 +1,6 @@
 source ~/stackrc
 uuids=$(openstack baremetal node list --format value -c UUID)
-node_type=yamaha
-disk=/dev/sda
+node_type=6048r
 mkdir ~/$node_type
 pushd ~/$node_type > /dev/null
 for uuid in $uuids; do
@@ -12,7 +11,7 @@ for uuid in $uuids; do
     fi
 done
 popd > /dev/null
-for f in $(ls ~/$node_type/); do echo -n $f ; echo -n " "; cat ~/$node_type/$f | jq '.inventory.disks' | grep -B 4 $disk | grep -w wwn | awk {'print $2'} | sed -e s/,//g -e s/\"//g;  done > ~/uuid_to_wwn-${node_type}
+for f in $(ls ~/$node_type/); do echo -n $f ; echo -n " "; cat ~/$node_type/$f | jq '.inventory.disks' | grep root_disk -A 11 | grep wwn | head -1 | awk {'print $2'} | sed -e s/,//g -e s/\"//g;  done > ~/uuid_to_wwn-${node_type}
 while read UUID WWN ; do
       echo "Setting root disk on node $UUID with wwn $WWN"
       #openstack baremetal node unset $UUID --property root_device
