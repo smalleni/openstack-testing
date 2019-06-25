f8e8231c23875055f8475e31dcae58497823d0d4
Sai Sindhur Malleni <smalleni@redhat.com>
2019-06-25 15:18:03 -0400

    Update profile tagging script

--- node_type_to_profile.sh
+++ node_type_to_profile.sh
@@ -1,21 +1,19 @@
 source ~/stackrc
 declare -A node_to_profile
-node_to_profile[r620]=r620-compute
-node_to_profile[r630]=r630-compute
-node_to_profile[r730xd]=r730xd-compute
-node_to_profile[r930]=r930-compute
-node_to_profile[6018r]=6018r-compute
-node_to_profile[6048r]=6048r-compute
+node_to_profile[5039ms]=5039ms-compute
+node_to_profile[1029p]=1029p-compute
 for node_type in "${!node_to_profile[@]}"
-    do 
+    do
         echo "Setting profile for node type $node_type"
         for i in $(openstack baremetal node list --format value -c UUID)
-            do  
-                node=$(openstack baremetal node show $i | grep driver_info | grep $node_type)
+            do
+                node=$(openstack baremetal node show $i --fields driver_info -f json | jq '.driver_info.ipmi_address' | grep $node_type)
                 if [ "$node" != "" ]
                     then
-                        echo "Updating node $i with profile ${node_to_profile[$node_type]}"
-                        openstack baremetal node set $i --property capabilities=profile:${node_to_profile[$node_type]},boot_option:local
+                        echo "Updating node $i with profile ${node_to_profile[$node_type]} and ip $(openstack baremetal node show $i --fields driver_info -f json | jq '.driver_info.ipmi_address')"
+                        openstack baremetal node set $i --property capabilities=profile:${node_to_profile[$node_type]},cpu_vt:true,cpu_hugepages:true,boot_option:local,cpu_txt:true,cpu_aes:true,cpu_hugepages_1g:true,boot_mode:bios
+
                 fi
             done
     done
+
